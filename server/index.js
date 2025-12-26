8016a8c78fd6030f958131f355283e2db2a64455
jdeinane <jenniferdianeu@gmail.com>
2025-12-26 21:42:03 +0100

    feat: add command to script to launch prod mode

--- server/index.js
+++ server/index.js
@@ -29,11 +29,11 @@ const io = new Server(server, {
 });
 
 /* Serving client files (PROD MODE) */
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+app.get(/^.*$/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 
 /**
